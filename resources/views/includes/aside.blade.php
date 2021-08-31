aff2fa4e951674da48e30b3a4fd84b35c61291d7
Denny Djaman <denny.d@angkasapura-supports.com>
2021-08-31 16:07:54 +0700

    commit all item

---
+++ resources/views/includes/aside.blade.php
@@ -0,0 +1,40 @@
+<section>
+    <div class="text-center text-md-left mt-5 ml-5">
+      <div class="row">
+        <div class="col-md-3 mx-auto mb-4">
+          <h6 class="text-uppercase font-weight-bold">Overview</h6>
+          <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 120px; height: 2px;">
+          <p class="mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore error illo id molestias, ipsa corrupti corporis repellat quo eum harum et similique esse eos, consectetur rem? Debitis nesciunt in eligendi?</p>
+        </div>
+        <div class="col-md-3 mx-auto mb-4">
+          <h6 class="text-uppercase font-weight-bold">Products</h6>
+          <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 85px; height: 2px;">
+          <ul class="list-unstyled">
+            <li class="my-2"><a href="#">Web Design</a></li>
+            <li class="my-2"><a href="#">Photography</a></li>
+            <li class="my-2"><a href="#">Architect</a></li>
+            <li class="my-2"><a href="#">Weading Organaizer</a></li>
+          </ul>
+      </div>
+      <div class="col-md-3 mx-auto mb-4">
+        <h6 class="text-uppercase font-weight-bold">Usefull Links</h6>
+        <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 125px; height: 2px;">
+        <ul class="list-unstyled">
+          <li class="my-2"><a href="#">Home</a></li>
+          <li class="my-2"><a href="#">About</a></li>
+          <li class="my-2"><a href="#">Service</a></li>
+          <li class="my-2"><a href="#">Contact</a></li>
+        </ul>
+    </div>
+    <div class="col-md-3 mx-auto mb-4">
+      <h6 class="text-uppercase font-weight-bold">Address</h6>
+      <hr class="bg-success mb-4 mt-0 d-inline-block mx-auto" style="width: 115px; height: 2px;">
+      <ul class="list-unstyled">
+        <li class="my-2"><i class="fas fa-home"></i> Jl. Apa katu nomor 7</li>
+        <li class="my-2"><i class="far fa-envelope"></i> admin@tujuid.com</li>
+        <li class="my-2"><i class="fas fa-phone"></i> +3453 985823</li>
+        <li class="my-2"><i class="fas fa-print"></i> 0000528328523</li>
+      </ul>
+  </div>
+    </div>
+  </section>
